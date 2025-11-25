f34802a2f178a0d3246e888920585952c26e2de8
Tarun15151 <tarun.24scse1010926@galgotiasuniversity.ac.in>
2025-11-25 12:47:07 +0530

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,29 +1,32 @@
-public class Car {
-    private int id;
-    private String brand;
-    private String model;
+package com.carrental;
+
+// Car extends Vehicle and implements Rentable (interface)
+public class Car extends Vehicle implements Rentable {
+    private int seats;
     private boolean available;
 
-    public Car(int id, String brand, String model, boolean available) {
-        this.id = id;
-        this.brand = brand;
-        this.model = model;
-        this.available = available;
+    public Car(int id, String make, String model, int year, double dailyRate, int seats) {
+        super(id, make, model, year, dailyRate);
+        this.seats = seats;
+        this.available = true;
     }
 
-    public int getId() {
-        return id;
+    public int getSeats() { return seats; }
+    public boolean isAvailable() { return available; }
+
+    public void setAvailable(boolean available) { this.available = available; }
+
+    @Override
+    public String getType() { return "Car"; }
+
+    @Override
+    public synchronized void rent() throws CarNotAvailableException {
+        if (!available) throw new CarNotAvailableException("Car " + id + " is not available.");
+        available = false;
     }
 
-    public String getBrand() {
-        return brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public boolean isAvailable() {
-        return available;
+    @Override
+    public synchronized void returns() {
+        available = true;
     }
 }
